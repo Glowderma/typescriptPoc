49b392d99057034dc50778fc847ef1a94c3ab493
anupama <anupama.kushwaha@tricos.co.in>
2024-08-13 09:43:01 +0530

    changes 13-aug

--- src/app.ts
+++ src/app.ts
@@ -1,108 +1,45 @@
-//import express from 'express';
-// import indexRouter from './routes/index.js';
-// import {auth, authLocal, verifyAdminRoutes} from "./middlewares/auth.js";
-// //import cors from "cors";
-// import {api} from "./utils/env.js";
-// import assetRouter from "./routes/asset.routes.js";
-// import rateLimit from 'express-rate-limit';
-
-
-//const app = express();
-//app.disable("x-powered-by");
-
-// const corsOptions = {
-//     origin: ['https://p360-staging-web-xrmnq.ondigitalocean.app', 'https://p360-prod-web-pwye6.ondigitalocean.app', 'https://p360.clouce.com']
-// };
-// app.use(cors(corsOptions));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-
-// const rateLimiterUsingThirdParty = rateLimit({
-//     windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-//     max: 10000,
-//     message: 'You have exceeded the 10000 requests in 24 hrs limit!', 
-//     standardHeaders: true,
-//     legacyHeaders: false,
-// });
-
-// app.use(rateLimiterUsingThirdParty);
-
-// if(api.nodeEnv !== "local" && api.nodeEnv !== "test") {
-//     app.use(auth);
-//    // app.use(verifyAdminRoutes);
-// } else {
-//     app.use(authLocal);
-// }
-
-// app.use('/', indexRouter);
-// app.use('/assets', assetRouter);
-
-// app.all('*', function (req, res) {
-//     res.status(404).json({
-//         statusId : "0",
-//         message : req.method + " to " + req.url + " does not exist",
-//         resbody : null
-//     });
-// });
-
-
-// export default app;
-
-
-import express from "express";
-const app = express();
-import indexRouter from './routes/index';
-import {auth, authLocal,verifyAdminRoutes} from "./middlewares/auth";
-import {api} from "./utils/env";
+import express, { Express } from "express";
+import indexRouter from "./routes/index";
+import { auth, authLocal, verifyAdminRoutes } from "./middlewares/auth";
+import { api } from "./utils/env";
 import apiRouter from "./routes/allApi.routes";
-import rateLimit from 'express-rate-limit';
-//import dotenv from "dotenv";
-//import axios from "axios";
-
+import rateLimit from "express-rate-limit";
 import cors from "cors";
-//import { Pool } from "pg";
-//import winston from "winston";
-//import cron from "node-cron";
-//import jwt from "jsonwebtoken";
-//import jwksRsa from "jwks-rsa";
-//import Joi from "joi";
-//import path from "path";
-//import fs from "fs";
-//import { createObjectCsvWriter } from "csv-writer";
-//dotenv.config();
 
-//const port = process.env.PORT;
+const createApp = (): Express => {
+  const app = express();
 
-app.use(cors());
-app.use(express.json());
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
 
-app.use(express.urlencoded({ extended: false }));
+  const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 100,
+    message: "Too many login attempts from this IP, please try again later.",
+  });
+  app.use(limiter);
 
+  app.use("/", indexRouter);
 
-const limiter = rateLimit({
-  windowMs: 60 * 1000, 
-  max: 100, 
-  message: "Too many login attempts from this IP, please try again later.",
-});
-app.use(limiter);
-app.use('/', indexRouter);
- if(api.nodeEnv !== "local" && api.nodeEnv !== "test") {
+  if (api.nodeEnv !== "local" && api.nodeEnv !== "test") {
     app.use(auth);
-     app.use(verifyAdminRoutes);
- } else {
-     app.use(authLocal);
- }
+    app.use(verifyAdminRoutes);
+  } else {
+    app.use(authLocal);
+  }
 
+  app.use("/api", apiRouter);
 
-app.use('/api', apiRouter);
-
-app.all('*', function (req, res) {
+  app.all("*", (req, res) => {
     res.status(404).json({
-        statusId : "0",
-        message : req.method + " to " + req.url + " does not exist",
-        resbody : null
+      statusId: "0",
+      message: `${req.method} to ${req.url} does not exist`,
+      resbody: null,
     });
-});
+  });
 
+  return app;
+};
 
-export default app;
+export default createApp;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
-import app from './app';
-import { api } from "./utils/env";
-import logger from "./utils/logger";
+import createApp from './app';
+import cluster from "cluster";
+import os from "os";
 
-app.listen(api.port, (err?: Error) => {
-    if (err) {
-        logger.error(err);
-    } else {
-        logger.info(`Successfully launched in ${api.nodeEnv} environment on port ${api.port}`);
-    }
-});
+if (cluster.isMaster) {
+  const numCPUs = os.cpus().length;
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  // When a worker dies, log it and start a new one
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  // Worker process logic: create and start the server
+  const app = createApp();
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Worker ${process.pid} started, Server running at http://localhost:${port}`);
+  });
+}
